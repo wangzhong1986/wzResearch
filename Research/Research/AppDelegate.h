c36a069df2032ba7c181e854b694403cc2b6c92b
wangzhong <megui.wz@120yibao.com>
2017-02-15 15:28:56 +0800

    init project

---
+++ Research/Research/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Research
+//
+//  Created by wangzhong on 2017/2/15.
+//  Copyright © 2017年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Research/Research/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Research
+//
+//  Created by wangzhong on 2017/2/15.
+//  Copyright © 2017年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
